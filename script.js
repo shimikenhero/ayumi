848be8201cd3465a62d71c5fb2fadc7d228448c5
shimikenhero <shimizu04011348@icloud.com>
2026-03-09 00:38:20 +0900

    update design

--- script.js
+++ script.js
@@ -1,57 +1,60 @@
-/* 背景写真リスト */
+/* 写真リスト */
 
-const images = [
+const images=[
 "2856C812-51A6-4756-8D68-9FCB0AA09324.JPEG",
 "IMG_9268.JPEG",
 "IMG_9325.JPEG",
 "IMG_9772.JPEG"
 ];
 
-let current = 0;
+let current=0;
+
+const bg=document.getElementById("background");
 
 function changeBackground(){
 
-document.body.style.backgroundImage =
-"url('" + images[current] + "')";
+bg.style.backgroundImage=
+"url('"+images[current]+"')";
 
 current++;
 
-if(current >= images.length){
+if(current>=images.length){
 
-current = 0;
-
-}
+current=0;
 
 }
 
-/* 最初の画像 */
+}
+
+/* 最初 */
 
 changeBackground();
 
-/* 5秒ごとに変更 */
+/* 5秒ごと */
 
 setInterval(changeBackground,5000);
 
 
-/* タイプライター文字 */
 
-const text =
+/* タイプライター */
+
+const text=
 "4ヶ月記念日おめでとう。\nいつもありがとう。\nこれからもよろしくね。\n大好きだよ。";
 
-let i = 0;
+let i=0;
 
-const speed = 80;
+const speed=80;
 
 function typeWriter(){
 
-if(i < text.length){
+if(i<text.length){
 
 document.getElementById("message").innerHTML
-+= text.charAt(i);
++=text.charAt(i);
 
 i++;
 
-setTimeout(typeWriter, speed);
+setTimeout(typeWriter,speed);
 
 }
 
@@ -60,21 +63,22 @@ setTimeout(typeWriter, speed);
 typeWriter();
 
 
-/* ハート演出 */
+
+/* ハート */
 
 function createHeart(){
 
-const heart = document.createElement("div");
+const heart=document.createElement("div");
 
 heart.classList.add("heart");
 
 heart.innerHTML="❤";
 
-heart.style.left = Math.random()*100+"vw";
+heart.style.left=Math.random()*100+"vw";
 
-heart.style.bottom = "-20px";
+heart.style.bottom="-20px";
 
-heart.style.fontSize = (15+Math.random()*25)+"px";
+heart.style.fontSize=(15+Math.random()*25)+"px";
 
 document.body.appendChild(heart);
 
@@ -82,8 +86,81 @@ setTimeout(()=>{
 
 heart.remove();
 
-},6000);
+},7000);
 
 }
 
-setInterval(createHeart,600);
+setInterval(createHeart,700);
+
+
+
+/* キラキラ */
+
+const canvas=document.getElementById("sparkle");
+
+const ctx=canvas.getContext("2d");
+
+canvas.width=window.innerWidth;
+
+canvas.height=window.innerHeight;
+
+let particles=[];
+
+for(let i=0;i<80;i++){
+
+particles.push({
+
+x:Math.random()*canvas.width,
+y:Math.random()*canvas.height,
+r:Math.random()*2+1,
+d:Math.random()*1
+
+});
+
+}
+
+function draw(){
+
+ctx.clearRect(0,0,canvas.width,canvas.height);
+
+ctx.fillStyle="white";
+
+ctx.beginPath();
+
+for(let i=0;i<particles.length;i++){
+
+let p=particles[i];
+
+ctx.moveTo(p.x,p.y);
+
+ctx.arc(p.x,p.y,p.r,0,Math.PI*2,true);
+
+}
+
+ctx.fill();
+
+update();
+
+}
+
+function update(){
+
+for(let i=0;i<particles.length;i++){
+
+let p=particles[i];
+
+p.y+=p.d;
+
+if(p.y>canvas.height){
+
+p.y=0;
+
+p.x=Math.random()*canvas.width;
+
+}
+
+}
+
+}
+
+setInterval(draw,33);
